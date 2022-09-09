ce83bfa7d8410596089459970f8f90d94fdb7ffe
Taiki Tsuboya <tsuboyataiki.411222@softbank.ne.jp>
2022-09-09 15:52:42 +0900

    ⚡️ ベースになるモデル追加

---
+++ db/schemas/station.schema.rb
@@ -0,0 +1,7 @@
+create_table "stations", id: :bigint, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  t.string "code",      null: false, limit: 100
+  t.float  "latitude",  null: false
+  t.float  "longitude", null: false
+
+  t.timestamps
+end
