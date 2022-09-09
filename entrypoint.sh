d80d7dfab23730696dd41bab0801e6bb8ebbee49
尾崎しょうご <syougo1999most@yahoo.co.jp>
2022-09-09 19:38:46 +0900

    entrypoint.shの修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,4 @@ cd /app
 bin/setup
 echo "finished setup"
 echo "migration"
-bundle exec ridgepole --apply --file ./db/schemas/Schemafile --config config/database.yml
-bundle exec rails db:seed
 bundle exec pumactl start
