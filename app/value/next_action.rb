ed3047673e4433c14e31468a8143851d1e8e00b9
尾崎しょうご <syougo1999most@yahoo.co.jp>
2022-09-12 19:14:02 +0900

    save

--- app/value/next_action.rb
+++ app/value/next_action.rb
@@ -1,4 +1,7 @@
 class NextAction
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
   attribute :method, :string
   attribute :price, :integer
   attribute :required_minute, :integer
