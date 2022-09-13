a212471d6e0b3381effca349e94bd62fb4b9c437
syougo1209 <syougo1999most@yahoo.co.jp>
2022-09-13 16:57:05 +0900

    feat: actions_controllerの処理の作成 (#11)
    * controller作成
    
    * arrived_atとleaved_atをつけるようにした
    
    * save

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,4 +1,7 @@
 class ActionsController < ApplicationController
+  # 渋谷区 35.666015, 139.698228
+  # 保土ヶ谷 35.465967, 139.570486
   def index
+    @full_taxi_plan = CreateFullTaxiPlanService.new(current_coordinates: [35.666015, 139.698228], home_coordinates: [35.465967, 139.570486]).call
   end
 end
--- app/services/create_full_taxi_plan_service.rb
+++ app/services/create_full_taxi_plan_service.rb
@@ -19,16 +19,16 @@ class CreateFullTaxiPlanService
   def create_full_taxi_route
     train = Train.new
     cost = TaxiCostCalculator.calc_cost(@current_coordinates, @home_coordinates)
-    minute = TimeCalculator.travel_minute(@current_coordinates, @home_coordinates, 40)
-    next_action = NextAction.new(method: 'taxi', price: cost, required_minute: minute , physical_point: 100, train: train)
+    @minute = TimeCalculator.travel_minute(@current_coordinates, @home_coordinates, 40)
+    next_action = NextAction.new(method: 'taxi', price: cost, required_minute: @minute , physical_point: 100, train: train)
 
-    Detail.new(name: '現在地', next_action: next_action)
+    Detail.new(name: '現在地', next_action: next_action, leaved_at: Time.current, arrived_at: nil)
   end
 
   def create_home_route
     train = Train.new
     next_action = NextAction.new(train: train)
-
-    Detail.new(name: '自宅', next_action: next_action)
+    arrived_at = Time.current + @minute * 60
+    Detail.new(name: '自宅', next_action: next_action, leaved_at: nil, arrived_at: arrived_at )
   end
 end
