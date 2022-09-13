88c002dc359eff8fdc3cd4d2c463a8a6f8c87681
尾崎しょうご <syougo1999most@yahoo.co.jp>
2022-09-13 20:12:52 +0900

    logic完成

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -3,5 +3,6 @@ class ActionsController < ApplicationController
   # 保土ヶ谷 35.465967, 139.570486
   def index
     @full_taxi_plan = CreateFullTaxiPlanService.new(current_coordinates: [35.666015, 139.698228], home_coordinates: [35.465967, 139.570486]).call
+    @hotel_to_home_plan = CreateHotelToHomePlanService.new(current_coordinates: [35.666015, 139.698228], home_coordinates: [35.465967, 139.570486]).call
   end
 end
--- app/services/create_hotel_to_home_plan_service.rb
+++ app/services/create_hotel_to_home_plan_service.rb
@@ -12,7 +12,7 @@ class CreateHotelToHomePlanService
     detail3 = create_train_detail
     detail4 = create_home_nearest_station_detail
 
-    Plan.new(details: [detail1, detail2, detail3])
+    Plan.new(details: [detail1, detail2, detail3, detail4])
   end
 
   private
@@ -29,22 +29,23 @@ class CreateHotelToHomePlanService
     minute = TimeCalculator.travel_minute(@current_nearest_station, @hotel, 4)
     train = Train.new
     next_action = NextAction.new(method: 'walk', price: 0, required_minute: minute , physical_point: 10, train: train)
-    Detail.new(name: @hotel.name, next_action: next_action, leaved_at: nil, arrived_at: nil)
+    Detail.new(name: @hotel.name, price: @hotel.price, next_action: next_action, leaved_at: nil, arrived_at: nil)
   end
 
   def create_train_detail
     home_nearest_station = Station.near(@home_coordinates, 10, unit: :km).first
 
-    @train_route = TrainRouteClient.request(station_code_from: @current_nearest_station.code, station_code_to: home_nearest_station.code)
-    train = Train.new(line: @train_route[0].lines[0].name, direction: @train_route[0].lines[0].direction, track: @train_route[0].lines[0].track)
-    required_minute = @train_route[0].arrived_at - @train_route[0].leaved_at
+    @train_route = TrainRouteClient.request(station_code_from: @current_nearest_station.code, station_code_to: home_nearest_station.code, is_next_day: true)
+    train = Train.new(line: @train_route[0][:lines][0][:name], direction: @train_route[0][:lines][0][:direction], track: @train_route[0][:lines][0][:track])
+    required_minute = 60
 
-    next_action = NextAction.new(method: 'train', price: 0, required_minute: required_minute, physical_point: 200, train: train)
-    Detail.new(name: @current_nearest_station.name, next_action: next_action, leaved_at: nil, arrived_at: @train_route[0].leaved_at)
+    next_action = NextAction.new(method: 'train', price: @train_route[0][:lines][0][:price], required_minute: required_minute, physical_point: 200, train: train)
+    Detail.new(name: @current_nearest_station.name, next_action: next_action, leaved_at: nil, arrived_at: @train_route[0][:leave_at])
   end
 
   def create_home_nearest_station_detail
-    next_action = NextAction.new
-    Detail.new(name: @train_route.last.to.station_name, next_action: next_action, leaved_at: nil, arrived_at: @train_route[0].arrived_at)
+    train = Train.new
+    next_action = NextAction.new(train: train)
+    Detail.new(name: @train_route.last[:to][:station_name], next_action: next_action, leaved_at: nil, arrived_at: @train_route[0][:arrive_at])
   end
 end
