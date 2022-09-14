cd072b8901678ce52c4f882f55d933914caad8c9
Yamamoto Tatsuya <dasixi34@gmail.com>
2022-09-14 14:11:28 +0900

    feat: 電車とタクシーを使うプランを追加

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -5,9 +5,10 @@ class ActionsController < ApplicationController
 
     full_train_plan = CreateFullTrainPlanService.new(current_coordinates: current_coordinates, home_coordinates: home_coordinates).call
     full_taxi_plan = CreateFullTaxiPlanService.new(current_coordinates: current_coordinates, home_coordinates: home_coordinates).call
+    train_taxi_plan = CreateTrainTaxiPlanService.new(current_coordinates: current_coordinates, home_coordinates: home_coordinates).call
     train_hotel = CreateTrainHotelService.new(current_coordinates: current_coordinates, home_coordinates: home_coordinates).call
     hotel_to_home_plan = CreateHotelToHomePlanService.new(current_coordinates: current_coordinates, home_coordinates: home_coordinates).call
 
-    @plans = [full_train_plan, full_taxi_plan, train_hotel, hotel_to_home_plan]
+    @plans = [full_train_plan, full_taxi_plan, train_taxi_plan, train_hotel, hotel_to_home_plan]
   end
 end
